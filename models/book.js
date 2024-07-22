7875f87bdb8636cb9a32bb49ebb844d0a235cadd
zainabdhaif <108354207+zainabdhaif@users.noreply.github.com>
2024-07-22 10:46:34 +0300

    updated schema

--- models/book.js
+++ models/book.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const bookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    rating: Integer,
-    yearPublished: Integer,
-    noOfPages: Integer,
-    publisher: String,
-    readYet: Boolean,
+    title:  { type: String, required: true },
+    author: { type: String, required: true },
+    rating:{ type: Number, required: true },
+    yearPublished: { type: Number, required: true },
+    noOfPages:{ type: Number, required: true },
+    publisher: { type: String, required: true },
+    readYet: { type: Boolean, required: true },
 });
 
 const Book = mongoose.model("Book", bookSchema);
--- server.js
+++ server.js
@@ -8,4 +8,17 @@ dotenv.config();
 //REQUIRE DATABASE
 require('./config/database');
 
+const Book = require("./models/book.js");
+
 const app = express();
+
+
+//MIDDLEWARE
+app.use(morgan("dev"));
+
+//ROUTES
+//landing page - just to check if server is working
+app.listen(5010, () => {
+    console.log("Listening on port 5010");
+  });
+  
